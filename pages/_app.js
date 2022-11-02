c64ef56db68b93fc89240e61b81a9ddb17542997
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-11-02 13:06:31 +0100

    Added apolloProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
+
+import client from '../apollo-client';
 import { GlobalStyle } from '../styles/GlobalStyles';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
 }
 
