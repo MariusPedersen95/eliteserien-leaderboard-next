050ecf0a12883c8f1a91935e97be0ca60d814c4f
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-11-02 18:39:50 +0100

    Client for apollo-client

--- apollo-client.js
+++ apollo-client.js
@@ -1,9 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  //
   uri: 'https://resultatservice-api.stage-sumo.tv2.no/api/graphql',
-  // uri: '',
   cache: new InMemoryCache(),
 });
 
